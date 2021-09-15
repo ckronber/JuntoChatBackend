8bde1f7e8c233aa55c4bfbfd86d5d418863b133f
Chris Kronberg <ckronber@gmail.com>
2021-09-14 21:06:05 -0400

    Created a REST API from tutorial

---
+++ Flask REST Api Tutorial/test.py
@@ -0,0 +1,14 @@
+import  requests
+
+Base = "http://127.0.0.1:5000/"
+
+#data = [ {"likes":78,"name":"Chris","views":1000000},  
+#         {"likes":56748,"name":"How to make REST API","views":10000000},  
+#         {"likes":4567,"name":"my 3rd video","views":5482158}]
+#for i in range(len(data)):
+#    response = requests.put(Base+"video/"+str(i),data[i])
+#    print(response.json())
+
+
+response = requests.patch(Base+"video/2", {"views":99,"likes":101})
+print(response.json())
--- TestApp/views.py
+++ TestApp/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,jsonify,redirect
+from flask.helpers import url_for
 
 views = Blueprint(__name__,"/")
 
@@ -6,6 +7,31 @@ views = Blueprint(__name__,"/")
 def home():
     return render_template("index.html", name = "Chris")
 
-@views.route('/profile/<username>')
-def profile(username):
-    return render_template("index.html", name = username)
+
+#returning an html file
+@views.route('/profile1')
+def profile1():
+    args = request.args
+    name = args.get('name')
+    return render_template("index.html", name = name)
+
+#returning json
+@views.route("/json")
+def get_json():
+    return jsonify({"Name":"Chris","Awesomeness":["A","B"]})
+
+
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+#redirect
+@views.route("/go-to-home")
+def goHome():
+    return redirect(url_for("views.get_json"))
+
+
+@views.route('/profile')
+def profile():
+    return render_template("profile.html")
